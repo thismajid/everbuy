8bf7f77050f7d3a7e2be0cf15a3c1116986633a3
Majid Khorshidian <mkhorshidian72@gmail.com>
2023-11-19 12:00:16 +0330

    Add global configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { APP_PORT } from './config/global.config';
+import { logger } from './common/utils/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(APP_PORT, () => {
+    logger.log(`App started on PORT ${APP_PORT} ... 🚀🔥`);
+  });
 }
 bootstrap();

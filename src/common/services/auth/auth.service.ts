676ca9acf36fc74606279bfa64a7093093ff736e
majid khorshidian <m.khorshidian@amnmoj.local>
2023-11-26 12:43:12 +0330

    Fixed development login otp env

--- src/common/services/auth/auth.service.ts
+++ src/common/services/auth/auth.service.ts
@@ -1,12 +1,12 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { DEFAULT_OTP, NODE_ENV } from 'src/config/global.config';
+import { DEVELOPMENT_LOGIN_OTP, NODE_ENV } from 'src/config/global.config';
 
 @Injectable()
 export class AuthService {
   generateOtp(length: number = 4): string {
     let otp = '';
     if (NODE_ENV !== 'production') {
-      otp = DEFAULT_OTP ?? '7777';
+      otp = DEVELOPMENT_LOGIN_OTP ?? '7777';
     } else {
       for (let i = 0; i < length; i++) {
         otp += Math.floor(Math.random() * 10);
--- src/config/global.config.ts
+++ src/config/global.config.ts
@@ -1,4 +1,4 @@
 import { config } from 'dotenv';
 
 config();
-export const { APP_PORT, NODE_ENV, DEFAULT_OTP } = process.env;
+export const { APP_PORT, NODE_ENV, DEVELOPMENT_LOGIN_OTP } = process.env;

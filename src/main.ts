9fff951ad35a80ceb74def7311233cee09d427c2
majid khorshidian <m.khorshidian@amnmoj.local>
2023-12-19 12:55:30 +0330

    Add response interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { AppModule } from './modules/app.module';
 import { APP_PORT } from './config/global.config';
 import SwaggerConfig from './config/swagger.config';
 import { logger } from './common/utils';
+import { ResponseInterceptor } from './common/interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableShutdownHooks();
   SwaggerConfig(app);
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(APP_PORT, () => {
     logger.debug(`App started on PORT ${APP_PORT} ... 🚀`);
   });

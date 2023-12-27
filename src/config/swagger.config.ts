c8038c54ebe4ab52d855d1d641a863b9e7ee8177
Majid Khorshidian <mkhorshidian72@gmail.com>
2023-12-27 17:11:01 +0330

    Fixed swagger order

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,6 +1,7 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { SecuritySchemeObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+import { APP_PORT } from './global.config';
 
 export default function SwaggerConfig(app: NestExpressApplication) {
   const config = new DocumentBuilder()
@@ -8,9 +9,15 @@ export default function SwaggerConfig(app: NestExpressApplication) {
     .setDescription('Everbuy api')
     .addBearerAuth(SwaggerBearerConfig())
     .setVersion('v1')
+    .addServer(`http://localhost:${APP_PORT}`, 'Development server')
     .build();
   const SwaggerDocument = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/docs', app, SwaggerDocument);
+  SwaggerModule.setup('v1/docs', app, SwaggerDocument, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+    },
+    customSiteTitle: 'Everbuy API Documentation',
+  });
 }
 function SwaggerBearerConfig(): SecuritySchemeObject {
   return {

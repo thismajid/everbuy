5060a8fd43108c532f2855354f4276868bb66c6b
Majid Khorshidian <mkhorshidian72@gmail.com>
2023-12-28 16:40:24 +0330

    Fixed user and cart models

---
+++ src/prisma/migrations/20231228072537_fixed_cart_model/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE `Cart` MODIFY `firstName` VARCHAR(191) NULL,
+    MODIFY `lastName` VARCHAR(191) NULL,
+    MODIFY `mobile` VARCHAR(191) NULL,
+    MODIFY `email` VARCHAR(191) NULL,
+    MODIFY `line1` VARCHAR(191) NULL,
+    MODIFY `line2` VARCHAR(191) NULL,
+    MODIFY `city` VARCHAR(191) NULL,
+    MODIFY `province` VARCHAR(191) NULL,
+    MODIFY `country` VARCHAR(191) NULL;
---
+++ src/prisma/migrations/20231228090252_fixed_deleted_at_cart_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Cart` MODIFY `deletedAt` DATETIME(3) NULL;
